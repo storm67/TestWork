c800d3badd0e0c044036686dcb23e2661987fbe2
storm67 <64145678+storm67@users.noreply.github.com>
2021-04-29 20:09:49 +0300

    First commit

---
+++ SimbirSoft/Service/RealmManager.swift
@@ -0,0 +1,9 @@
+//
+//  RealmManager.swift
+//  SimbirSoft
+//
+//  Created by Storm67 on 29/04/2021.
+//  Copyright © 2021 Storm67. All rights reserved.
+//
+
+import Foundation
---
+++ SimbirSoft/Service/RealmService.swift
@@ -0,0 +1,9 @@
+//
+//  RealmService.swift
+//  SimbirSoft
+//
+//  Created by Storm67 on 29/04/2021.
+//  Copyright © 2021 Storm67. All rights reserved.
+//
+
+import Foundation
